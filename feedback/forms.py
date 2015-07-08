698a347affe07772e7c55e84def99af74d2bbaff
Michael Herman <hermanmu@gmail.com>
2015-07-07 21:45:39 -0600

    version 3

---
+++ feedback/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    email = forms.EmailField(label="Email Address")
+    message = forms.CharField(
+        label="Message", widget=forms.Textarea(attrs={'rows': 5}))
+    honeypot = forms.CharField(widget=forms.HiddenInput(), required=False)
